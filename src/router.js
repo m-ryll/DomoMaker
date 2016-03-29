17d0a696248c51f6391754aee26fbe33a18192ae
Mandy Ryll <alr9596@rit.edu>
2016-03-28 23:34:25 -0400

    IT WAS A TYPO

--- src/router.js
+++ src/router.js
@@ -2,10 +2,10 @@ var controllers = require("./controllers");
 var mid = require("./middleware/index.js");
 
 var router = function(app) {
-	app.get("/login", mid.requresSecure, mid.requiresLogout, controllers.Account.loginPage);
-	app.post("/login", mid.requresSecure, mid.requiresLogout, controllers.Account.login);
-	app.get("/signup", mid.requresSecure, mid.requiresLogout, controllers.Account.signupPage);
-	app.post("/signup", mid.requresSecure, mid.requiresLogout, controllers.Account.signup);
+	app.get("/login", mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
+	app.post("/login", mid.requiresSecure, mid.requiresLogout, controllers.Account.login);
+	app.get("/signup", mid.requiresSecure, mid.requiresLogout, controllers.Account.signupPage);
+	app.post("/signup", mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
 	app.get("/logout", mid.requiresLogin, controllers.Account.logout);
 	app.get("/maker", mid.requiresLogin, controllers.Domo.makerPage);
 	app.post("/maker", mid.requiresLogin, controllers.Domo.make);
